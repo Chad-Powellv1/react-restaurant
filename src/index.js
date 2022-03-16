7cee16f9c0af6896bdd97bb1ac85313600661b10
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-16 20:36:51 +0000

    udpate navbar build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={}></Route>
+      <Route path='' element={}></Route>
+      <Route path='' element={}></Route>
+      <Route path='' element={}></Route>
+    </Routes>
+  
+  
+  
+  
+  </BrowserRouter>
+
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
