a38e60077ec5e88ae504c207b779dd3063f13e4d
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-17 01:03:46 +0000

    navbar and routes completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
+import { Children } from 'react';
 import './App.css';
-
+import { NavComponent } from './components/NavComponent'
 
 
 function App() {
   return (
     <div className="App">
-   
+      <NavComponent />
     </div>
   );
 }
