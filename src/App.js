8cc7e9205b87e716016b5d3132dc7660db298a01
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-18 14:25:25 +0000

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App({children}) {
     <div className="App">
       <Navbar className='navbar navbar-expand-lg' expand="lg">
         <Container className='container-fluid mb-5'>
-            <Navbar.Brand href="/" className='navbarBrand sm-none position-absolute top-0 start-0 ps-5 mt-2'><img className='img' src={cafe} alt="Cafe Logo" width= '300' /></Navbar.Brand>
+            <Navbar.Brand href="/" className='navbarBrand sm-none position-absolute top-0 start-0 ps-5 mt-2'><img src={cafe} alt="Cafe Logo" width= '300' /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav text-start">
             <Nav className="mx-auto text-center mt-2 ps-5 mt-5 pt-4 fs-4">
