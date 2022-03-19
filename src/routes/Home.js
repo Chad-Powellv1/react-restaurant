808ffa0df76f8caa62afe0e8089d04bb4f276ef0
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-19 19:48:23 +0000

    fix hamburger menu not showing and image not hiding on mobile

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../App';
-
+import './home.css'
 
 export default function HOME() {
 	return (
-      <Layout>
+		<Layout>
 			<div className='container mt-5'>
-				<div className='row'>
-					<h1 className='display-1 text-center text-danger text-decoration-underline fw-bold mt-3'>
+				<div className='row mt-5'>
+					<h1 className='display-1 text-center text-danger fw-bold mt-5'>
 						Awesome Thyme Cafe
 					</h1>
 					<br />
@@ -32,7 +32,13 @@ export default function HOME() {
 						<h4 className='phone text-light'>(859) 555-1212</h4>
 					</div>
 				</div>
+				<div class='map-responsive justify-content-center mt-5'>
+					<iframe
+						title="Awesome Inc Map Location"
+						src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3142.211231743101!2d-84.49472698391718!3d38.042164204501674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x884244e844532c33%3A0xb6919957af36b713!2s348%20E%20Main%20St%2C%20Lexington%2C%20KY%2040507!5e0!3m2!1sen!2sus!4v1647712784185!5m2!1sen!2sus'
+					></iframe>
+				</div>
 			</div>
-      </Layout>
-		)
+		</Layout>
+	);
 }
