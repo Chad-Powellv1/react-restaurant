f50534f530b151572b27d8ca508fe49d3d00981d
Chad Powell <chada.powell@outlook.com>
2022-07-14 14:47:30 -0400

    changed BrowserRouter to HashRouter for github pages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Specials from './routes/Specials';
-import { render } from 'react-dom';
-import Home from './routes/Home';
-import Menu from './routes/Menu';
-import React from 'react';
-import './index.css';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Specials from "./routes/Specials";
+import { render } from "react-dom";
+import Home from "./routes/Home";
+import Menu from "./routes/Menu";
+import React from "react";
+import "./index.css";
 
 render(
-	<BrowserRouter>
-		<Routes>
-			<Route path='/' element={<Home />} />
-			<Route path='menu' element={<Menu />} />
-			<Route path='specials' element={<Specials />} />
-		</Routes>
-	</BrowserRouter>,
-	document.getElementById('root')
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='menu' element={<Menu />} />
+      <Route path='specials' element={<Specials />} />
+    </Routes>
+  </Router>,
+  document.getElementById("root")
 );
